f4e19cbe047f72690e0a1045d0067a78a3aa61d1
paxsign <paxsign@gmail.com>
2019-04-28 11:25:58 +0700

    check type and extend
    Signed-off-by: paxsign <paxsign@gmail.com>

---
+++ checkType.js
@@ -0,0 +1,63 @@
+"use strict";
+
+function isFunction(func) {
+  if (typeof func == "function") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isArray(arr) {
+  if (Object.prototype.toString.call(arr) == "[object Array]") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isObject(obj) {
+  if (Object.prototype.toString.call(obj) == "[object Object]") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isNumber(num) {
+  if (typeof num == "number") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isString(str) {
+  if (typeof str == "string") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isEmpty(emp) {
+  if (typeof emp == "string" && emp.length == 0) {
+    return true;
+  } else {
+    if (
+      Object.prototype.toString.call(emp) == "[object Null]" ||
+      Object.prototype.toString.call(emp) == "[object Undefined]"
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+
+function extendClass (Parent, Child) {
+    for(var prop in Parent.prototype){
+        Child.prototype[prop]=Parent.prototype[prop];
+    }
+}
