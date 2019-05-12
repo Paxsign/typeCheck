bd53dc3c6772fc0084f81470aa011c737d703652
paxsign <paxsign@gmail.com>
2019-05-12 18:51:47 +0700

    code shortening
    Signed-off-by: paxsign <paxsign@gmail.com>

--- checkType.js
+++ checkType.js
@@ -1,56 +1,31 @@
 "use strict";
 
 function isFunction(func) {
-  if (typeof func == "function") {
-    return true;
-  } else {
-    return false;
-  }
+  return typeof func == "function";
 }
 
 function isArray(arr) {
-  if (Object.prototype.toString.call(arr) == "[object Array]") {
-    return true;
-  } else {
-    return false;
-  }
+  return Object.prototype.toString.call(arr) == "[object Array]";
 }
 
 function isObject(obj) {
-  if (Object.prototype.toString.call(obj) == "[object Object]") {
-    return true;
-  } else {
-    return false;
-  }
+  return  Object.prototype.toString.call(obj) == "[object Object]";
 }
 
 function isNumber(num) {
-  if (typeof num == "number") {
-    return true;
-  } else {
-    return false;
-  }
+  return  typeof num == "number";
 }
 
 function isString(str) {
-  if (typeof str == "string") {
-    return true;
-  } else {
-    return false;
-  }
+  return typeof str == "string";
 }
 
 function isEmpty(emp) {
   if (typeof emp == "string" && emp.length == 0) {
     return true;
-  } else {
-    if (
-      Object.prototype.toString.call(emp) == "[object Null]" || Object.prototype.toString.call(emp) == "[object Undefined]"
-    ) {
-      return true;
-    } else {
-      return false;
-    }
+  } 
+  else {
+    return Object.prototype.toString.call(emp) == "[object Null]" || Object.prototype.toString.call(emp) == "[object Undefined]";
   }
 }
 
